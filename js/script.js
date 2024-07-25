c1e79231abf9d67b39bf9f64283a3daf61672801
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-07-25 10:50:36 -0500

    Home sin footer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let currentSlide = 0;
 
 function moveSlide(n) {
-  const slides = document.querySelectorAll('.slide');
+  const slides = document.querySelectorAll('.home__slide');
   currentSlide = (currentSlide + n + slides.length) % slides.length;
-  document.querySelector('.slides').style.transform = `translateX(-${currentSlide * 100}%)`;
+  document.querySelector('.home__slides').style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
 setInterval(() => {
